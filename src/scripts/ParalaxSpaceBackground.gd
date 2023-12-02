ba30504be78af17d0ed629bffd85eff5ec52db21
= <reynolds552@gmail.com>
2023-12-01 23:00:35 -0800

    New platform type for Level 1

--- src/scripts/ParalaxSpaceBackground.gd
+++ src/scripts/ParalaxSpaceBackground.gd
@@ -4,5 +4,4 @@ extends ParallaxBackground
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print("Should Be Running", Time.get_ticks_msec())
 	scroll_offset.x -= scrolling_speed * delta
